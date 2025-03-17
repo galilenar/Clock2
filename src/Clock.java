53d8c132053fdde14019f075414525360777a6c7
galilenar <galilenar@yandex.ru>
2025-03-17 16:26:57 +0300

    j

--- src/Clock.java
+++ src/Clock.java
@@ -1,29 +1,11 @@
-class Time{
-    final private int min;
-    final private int hou;
-
-    public int getMin() {
-        return min;
-    }
-
-    public int getHou() {
-        return hou;
-    }
-    Time(int x, int y){
-        if (x>12) {hou=x-12;}
-            else {hou=x;}
-        min=y;
-
-    }
-}
-
 public class Clock {
-     private final static float mdegree=6f;
-     private final static float hdegree=30f;
+
+     private final static float MDEGREE =6f;
+     private final static float HDEGREE =30f;
      public static float  calcAngle(Time now){
-         float min1degree=mdegree*(now.getMin());
+         float min1degree= MDEGREE *(now.getMin());
          System.out.println("minutes= "+min1degree);
-         float hou1degree=hdegree*now.getHou()+mdegree*5*now.getMin()/60f;
+         float hou1degree= HDEGREE *now.getHou()+ MDEGREE *5*now.getMin()/60f;
          System.out.println("hours= "+hou1degree);
 
          float angle=Math.abs(hou1degree-min1degree);
@@ -38,5 +20,24 @@ public class Clock {
         //System.out.println("Hello!");
         Time now=new Time(0,30);
         System.out.println(calcAngle(now));
+    }
+
+    private static class Time{
+        final private int min;
+        final private int hou;
+
+        public int getMin() {
+            return min;
+        }
+
+        public int getHou() {
+            return hou;
+        }
+        public Time(int hou, int min){
+            if (hou>12) {this.hou=hou-12;}
+                else {this.hou=hou;}
+            this.min=min;
+
+        }
     }
 }
